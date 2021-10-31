fa6534b40f16f1c21aebb52f9d1014e2bdc2c533
periyakaruppan932000 <91140743+periyakaruppan932000@users.noreply.github.com>
2021-10-31 21:14:48 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,107 +1,115 @@
-package com.application;
+import java.util.*;
 
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
-import java.util.Scanner;
-import java.util.Set;
-
-public class Main {
-	
-	
+	public class Main {
 	static Map<String,String> logincr=new HashMap<String,String>();
-	
-	public static Map<String, String> LogInCredentials()
-	{
 		
-		
-		logincr.put("user1","user1@123");
-		logincr.put("user2","user2@123");
-		logincr.put("admin","admin@123");
-		return logincr;
-		
-		
-	}
-
-	
-	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		Guest g=new Guest();
-		User u=new User();
-		Admin a=new Admin();
-		
-		Scanner scan=new Scanner(System.in);
-		System.out.println("username:");
-		String uname=scan.nextLine();
-		System.out.println("Password:");
-		String pass=scan.nextLine();
-		Map<String,String> login=LogInCredentials();
-		
-		
-		Set<String> keys=logincr.keySet();////////get the key values from LogInCredentials and stores in keys
-		Iterator<String> it=keys.iterator();///////iterate over keys(keySet)
-		
-		while(it.hasNext())
+		public static Map<String, String> LogInCredentials()
 		{
-			String username=it.next();       //////////Get keys
 			
-			String password=logincr.get(username);   ///////////Get values for corresponding keys
 			
-			if(username.equals(uname)&&password.equals(pass))
-			{
-				System.out.println("Logged in successfully");
-				g.flag=true;
-				
-			}
+			logincr.put("user1","user1@123");
+			logincr.put("user2","user2@123");
+			logincr.put("admin","admin@123");
+			return null;
 			
-			if(uname.contains("admin"))
-			{
-				
-				a.flag=true;
-				
-				
-				System.out.println("1)Add food"+"\n"+"2)View Menu");
-				int n=scan.nextInt();
-				
-				switch(n)
-				{
-				case 1:
-					a.addFood();
-					break;
-		
-				case 2:
-					a.viewMenu();
-					
-					break;
-				
-				}
-				
-			}
-			
-			if(g.flag==false&&a.flag==false)
-			{
-				System.out.println("Account not found. Please register first...");
-				
-				String[] s=g.getRegistered();
-				for(int i=0;i<s.length;i++)
-				{
-					logincr.put(s[0], s[1]);
-				}
-				
-				System.out.println(logincr);
-				System.out.println("Account created successfully");
-				
-			}
 			
 		}
-		
-		
 
-	}
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			Scanner sc=new Scanner(System.in);
+			LogInCredentials();
+			System.out.println("Home page");
+			System.out.println("1.Register\n2.Login");
+			Admin a =new Admin();
+			Guest g=new Guest();
+			User u=new User();
+			int ab=sc.nextInt();
+			switch(ab)
+			{
+			case 1:
+					
+					//System.out.println("Account not found. Please register first...");
+					
+					String[] s=g.getRegistered();
+					for(int i=0;i<s.length;i++)
+					{
+						logincr.put(s[0], s[1]);
+					}
+					
+					System.out.println(logincr);
+					System.out.println("Account created successfully");
+					
+				
+				break;
+			case 2:
+				sc.nextLine();
+				System.out.println("username:");
+				String uname=sc.nextLine();
+				System.out.println("Password:");
+				String pass=sc.nextLine();
+				Map<String,String> login=LogInCredentials();
+				//System.out.println(login);
+				
+				Set<String> keys=logincr.keySet();////////get the key values from LogInCredentials and stores in keys
+				Iterator<String> it=keys.iterator();///////iterate over keys(keySet)
+				
+				while(it.hasNext())
+				{
+					String username=it.next();       //////////Get keys
+					
+					String password=logincr.get(username);   ///////////Get values for corresponding keys
+					//System.out.println(a.flag);
+					if(username.equals(uname)&&password.equals(pass))
+					{
+						System.out.println("Logged in successfully");
+						g.flag=true;
+						a.flag=true;
+						u.flag=true;
+						
+					}
+					//break;
+				}
+				
+				if(uname.contains("admin")&& a.flag==true)
+			    {
+			    	
+			    	while(a.flag==true)
+					{
+			    	System.out.println("1)Add food"+"\n"+"2)View Menu"+"\n"+"3)Exit");
+					int n=sc.nextInt();
+					
+					switch(n)
+					{
+					case 1:
+						a.addFood();
+						break;
+			
+					case 2:
+						a.viewMenu();
+						
+						break;
+						
+					case 3:
+						a.flag=false;
+						break;
+					
+					}	
+			    }
+					
+			    }
+				if(uname.contains("user")&&u.flag==true)
+				{
+					a.viewMenu();
+					//List<String> l2=new ArrayList<String>();
+				    u.orderfood();
+					u.addcart();
+					
+				}
+				break;
+			}
+			
+	        
+		}
 
-
-	}
-
-
+		}
